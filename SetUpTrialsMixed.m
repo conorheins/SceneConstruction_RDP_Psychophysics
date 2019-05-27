2b2eeddc70345051a0a6a73dbc0a69551cb334ff
Conor Heins <conor.heins@gmail.com>
2019-05-27 16:11:05 +0200

    more edits related to making it specific to current task

--- SetUpTrialsMixed.m
+++ SetUpTrialsMixed.m
@@ -3,17 +3,20 @@ function [myVar, block] = SetUpTrialsMixed(inf, myVar)
 % set all variables for running the blocks.
 % To match the study
 %
-% RDP LOCATION      : Upper left (Loc 1), Lower left (Loc 2), Upper right (Loc 3), Lower Right (Loc 4) 
-% RDP DIRECTION     : 0 degrees ('UP'), 90 degrees ('RIGHT'), 180 degrees
-%                     ('DOWN'), 270 degrees ('LEFT')
-% COHERENCE         : 5% (Low), 25% (Low-Medium), 50% (Medium), 75% (Medium High) 
-
+% RDP LOCATION                : Upper left (Loc 1), Lower left (Loc 2), Upper right (Loc 3), Lower Right (Loc 4) 
+% COHERENCE                   : 5% (Low), 25% (Low-Medium), 50% (Medium-High), 75% (High) 
+% RDP DIRECTION COMBINATIONS  : 0 degrees & 90 degrees ('UP RIGHT'); 90 degrees
+%                               & 180 degrees ('RIGHT DOWN'); 180 degrees & 270 degrees
+%                               ('DOWN LEFT'); 270 degrees & 0 degreess ('LEFT UP')
+                               
 %% Levels of variables
 %number of trials will be factor1*factor2*... * iterations
 
+numCoherLevels = 4;
+numScenes      = 4;
 
-factor.RDMcohers      = 2;
-factor.RDMdirections  = 2;
+factor.RDMcohers      = 2; % the number of values that a given RDP can take, per level/factor
+factor.scenes         = 2; % the number of values that a given RDP can take, per level/factor
 
 %Number of iterations and blocks
 factor.block          = 5;     % How many blocks do we have?
@@ -32,16 +35,17 @@ end
 
 factor.RDMconfigs = config_counter;
 
+RDM.cohers = zeros(numCoherLevels,factor.RDMcohers); 
+RDM.cohers(1,:)    = [5 5];
+RDM.cohers(2,:)    = [25 25];
+RDM.cohers(3,:)    = [50 50];
+RDM.cohers(4,:)    = [75 75];
 
-RDM.cohers(1).x    = [5 5];
-RDM.cohers(2).x    = [25 25];
-RDM.cohers(3).x    = [50 50];
-RDM.cohers(4).x    = [75 75];
-
-RDM.directions(1).x    = [0 90];
-RDM.directions(2).x    = [90 180];
-RDM.directions(3).x    = [180 270];
-RDM.directions(4).x    = [270 0];
+RDM.scenes = zeros(numScenes,factor.scenes); 
+RDM.scenes(1,:)    = [0 90];
+RDM.scenes(2,:)    = [90 180];
+RDM.scenes(3,:)    = [180 270];
+RDM.scenes(4,:)    = [270 0];
 
 %% create block struct
 block = struct;
@@ -51,9 +55,9 @@ for bl = 1:factor.block
     for config_i = 1:factor.RDMconfigs
 %         conSort = 1;
         for coh_i = 1:factor.RDMcohers
-            for scene_i = 1:factor.RDMdirections
+            for scene_i = 1:factor.scenes
                 tr = tr + 1;
-                block(bl).trials(tr).dotParams = createDotParams_struct(Scr.wRect,numPatterns,'centers',RDM.configs(config_i).x,'cohers',RDM.cohers(coh_i).x,'directions',RDM.directions(scene_i).x,...
+                block(bl).trials(tr).dotParams = createDotParams_struct(Scr.wRect,numPatterns,'centers',RDM.configs(config_i).x,'cohers',RDM.cohers(coh_i,:),'directions',RDM.scenes(scene_i,:),...
                     'speeds',[2 2],'apSizes',[200 200; 200 200],'nDots',[50 50]);
 %                 conSort = conSort + 1;
 %                 block(b).trials(tr).conSort = conSort;
@@ -64,8 +68,6 @@ end
 
 %% STIMULI GENERATOR
 for b = 1:factor.block
-
-    
 
     %-------------PreDefineVarisbles-------------%
     %Resp
