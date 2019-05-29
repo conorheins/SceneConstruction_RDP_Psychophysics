44195e8204f95569fdcdd19feb85ea1d098b6fc1
Conor Heins <conor.heins@gmail.com>
2019-05-29 09:31:27 +0200

    minor edits

--- RDP_testing_env.m
+++ RDP_testing_env.m
@@ -36,7 +36,7 @@ centers = [ Scr.wRect(3)/4, Scr.wRect(4)/4;
             Scr.wRect(3)/4, 3*Scr.wRect(4)/4];
         
 dotParams = createDotParams_struct(Scr.wRect,numPatterns,'centers',centers,'cohers',[80 80],'directions',[0 90],...
-            'speeds',[2 2],'apSizes',[200 200; 200 200],'nDots',[50 50]);
+            'speeds',[0.75 0.75],'apSizes',[200 200; 200 200],'nDots',[50 50],'lifetimes',[10 10]);
         
 
 % this function creates a dotParams structure array of the parameters used
@@ -58,9 +58,9 @@ dotParams = createDotParams_struct(Scr.wRect,numPatterns,'centers',centers,'cohe
 
 %% testing out a simple trial fuanction
 
-% scene_id = 1;
-% save_flag = false;
-% trial_data = run_trial_old(Scr,dotParams,scene_id,save_flag);
+scene_id = 1;
+save_flag = false;
+trial_data = run_trial_old(Scr,dotParams,scene_id,save_flag);
 
 
 %% Explanation:
@@ -68,84 +68,84 @@ dotParams = createDotParams_struct(Scr.wRect,numPatterns,'centers',centers,'cohe
 % in the structure array dotParams, using the index patt_id to create the
 % dotData for the relevant one
 
-numPatterns = 4;       
-centers = [ Scr.wRect(3)/4, Scr.wRect(4)/4;
-            Scr.wRect(3)/4, 3*Scr.wRect(4)/4;
-            3*Scr.wRect(3)/4, Scr.wRect(4)/4;
-            3*Scr.wRect(3)/4, 3*Scr.wRect(4)/4];
-        
-dotParams = createDotParams_struct(Scr.wRect,numPatterns,'centers',centers,'cohers',[5 15 25 75],'directions',[0 90 180 270],...
-            'speeds',[0.75 0.75 0.75 0.75],'apSizes',[200 200; 200 200; 200 200; 200 200],'nDots',[50 50 50 50]);
-        
-
-for patt_id = 1:numPatterns
-    dotData(patt_id) = initialize_dots(dotParams,patt_id);
-end
+% numPatterns = 4;       
+% centers = [ Scr.wRect(3)/4, Scr.wRect(4)/4;
+%             Scr.wRect(3)/4, 3*Scr.wRect(4)/4;
+%             3*Scr.wRect(3)/4, Scr.wRect(4)/4;
+%             3*Scr.wRect(3)/4, 3*Scr.wRect(4)/4];
+%         
+% dotParams = createDotParams_struct(Scr.wRect,numPatterns,'centers',centers,'cohers',[5 15 25 75],'directions',[0 90 180 270],...
+%             'speeds',[0.75 0.75 0.75 0.75],'apSizes',[200 200; 200 200; 200 200; 200 200],'nDots',[50 50 50 50]);
+%         
 % 
-% % 1, 2 and 3 draw round dots (circles) with
-% % anti-aliasing: 1 favors performance, 2 tries to use high-quality anti-aliasing,
-% % if supported by your hardware. 3 Uses a builtin shader-based implementation.
-% % dot_type 1 and 2 may not be supported by all graphics cards and drivers.
-% 
-numFlips = 1000;
-% 
-priorityLevel = MaxPriority(Scr.wRect,'KbCheck');
-Priority(priorityLevel);
-% 
-
-% video_dat = zeros(Scr.wRect(4)/2,Scr.wRect(3)/2,3,numFlips);
-
-for flip_i = 1:numFlips 
-
-    %% Explanation:
-    % update dots: this function will take the dotData (if flip_i == 1, it
-    % will take the dotData output from initialize_dots) and update the
-    % positions/lifetimes/etc.
-    for patt_i = 1:numPatterns
-        dotData(patt_i) = update_dots(dotData(patt_i));
-    end
-    
-    %     After all computations, flip
-    Screen('Flip', Scr.w,0,0);
-    
-    % draws the current dots, using position, single size argument and
-    % dotType
-    for patt_i = 1:numPatterns
-        Screen('DrawDots', Scr.w, dotData(patt_i).dotPos, dotData(patt_i).size, [255 255 255], [0 0], dotData(patt_i).dotType);
-    end
-    
-    %      Presentation
-    Screen('DrawingFinished',Scr.w,0);
-%     tmp = Screen('GetImage',Scr.w);
-%     video_dat(:,:,:,flip_i) = tmp(1:4:end,1:4:end,:);
-
-end
-% 
-% Present last dots
-Screen('Flip', Scr.w,0,0);
-
-% Erase last dots
-Screen('DrawingFinished',Scr.w,0);
-Screen('Flip', Scr.w,0,0);
-
-Screen('CloseAll'); % Close display windows
-Priority(0); % Shutdown realtime mode.
-
-% video_dat = video_dat./255; % normalize to between 0 and 1
-% 
-% for flip_i = 1:numFlips
-%     % Gifs can't take RBG matrices: they have to be specified with the
-%     % pixels as indices into a colormap
-%     % See the help for imwrite for more details
-%     [y, newmap] = cmunique(video_dat(:,:,:,flip_i));
-%     
-%     %Creates a .gif animation - makes first frame, then appends the rest
-%     if flip_i==1
-%         imwrite(y, newmap, 'RDP_example.gif');
-%     else
-%         imwrite(y, newmap, 'RDP_example.gif', 'DelayTime', 1/30, 'WriteMode', 'append');
-%     end
+% for patt_id = 1:numPatterns
+%     dotData(patt_id) = initialize_dots(dotParams,patt_id);
 % end
-%      
-    
-    
+% % 
+% % % 1, 2 and 3 draw round dots (circles) with
+% % % anti-aliasing: 1 favors performance, 2 tries to use high-quality anti-aliasing,
+% % % if supported by your hardware. 3 Uses a builtin shader-based implementation.
+% % % dot_type 1 and 2 may not be supported by all graphics cards and drivers.
+% % 
+% numFlips = 1000;
+% % 
+% priorityLevel = MaxPriority(Scr.wRect,'KbCheck');
+% Priority(priorityLevel);
+% % 
+% 
+% % video_dat = zeros(Scr.wRect(4)/2,Scr.wRect(3)/2,3,numFlips);
+% 
+% for flip_i = 1:numFlips 
+% 
+%     %% Explanation:
+%     % update dots: this function will take the dotData (if flip_i == 1, it
+%     % will take the dotData output from initialize_dots) and update the
+%     % positions/lifetimes/etc.
+%     for patt_i = 1:numPatterns
+%         dotData(patt_i) = update_dots(dotData(patt_i));
+%     end
+%     
+%     %     After all computations, flip
+%     Screen('Flip', Scr.w,0,0);
+%     
+%     % draws the current dots, using position, single size argument and
+%     % dotType
+%     for patt_i = 1:numPatterns
+%         Screen('DrawDots', Scr.w, dotData(patt_i).dotPos, dotData(patt_i).size, [255 255 255], [0 0], dotData(patt_i).dotType);
+%     end
+%     
+%     %      Presentation
+%     Screen('DrawingFinished',Scr.w,0);
+% %     tmp = Screen('GetImage',Scr.w);
+% %     video_dat(:,:,:,flip_i) = tmp(1:4:end,1:4:end,:);
+% 
+% end
+% % 
+% % Present last dots
+% Screen('Flip', Scr.w,0,0);
+% 
+% % Erase last dots
+% Screen('DrawingFinished',Scr.w,0);
+% Screen('Flip', Scr.w,0,0);
+% 
+% Screen('CloseAll'); % Close display windows
+% Priority(0); % Shutdown realtime mode.
+% 
+% % video_dat = video_dat./255; % normalize to between 0 and 1
+% % 
+% % for flip_i = 1:numFlips
+% %     % Gifs can't take RBG matrices: they have to be specified with the
+% %     % pixels as indices into a colormap
+% %     % See the help for imwrite for more details
+% %     [y, newmap] = cmunique(video_dat(:,:,:,flip_i));
+% %     
+% %     %Creates a .gif animation - makes first frame, then appends the rest
+% %     if flip_i==1
+% %         imwrite(y, newmap, 'RDP_example.gif');
+% %     else
+% %         imwrite(y, newmap, 'RDP_example.gif', 'DelayTime', 1/30, 'WriteMode', 'append');
+% %     end
+% % end
+% %      
+%     
+%     
