d6bd5bce0e3d6671dd72d358b9bb195965097ec9
conorheins <conor.heins@gmail.com>
2019-06-04 13:46:59 +0200

    more coh levels + corresponding pilot data

--- MotionDiscrim/RDP_psychometric_analysis.m
+++ MotionDiscrim/RDP_psychometric_analysis.m
@@ -75,8 +75,9 @@ end
 %%
 coh_axis = 0.1:0.1:100;
 
-labels = {'RCH','MZ','RV','EA'};
+% labels = {'RCH','MZ','RV','EA'};
 % labels = {'RCH2','RCH1'};
+labels = {'Masha','Dima'};
 
 colors = cool(ceil(1.5*length(labels)));
 colors = colors(1:length(labels),:);
