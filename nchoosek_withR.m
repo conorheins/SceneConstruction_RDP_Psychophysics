54b0e1594a50690eec76355785298939f839b3f0
Conor Heins <conor.heins@gmail.com>
2019-06-10 14:34:43 +0200

    edit

--- nchoosek_withR.m
+++ nchoosek_withR.m
@@ -1,7 +1,5 @@
 function [ numCombos ] = nchoosek_withR( N,K )
-%nchoosek_withR It's like MATLAB's native nchoosek but with replacements,
-%bitch!
-%   Detailed explanation goes here
+%nchoosek_withR It's like MATLAB's native nchoosek but with replacements
 
 numCombos = prod(1:(N + K - 1)) / (prod(1:K) * prod(1:(N-1)));
 
