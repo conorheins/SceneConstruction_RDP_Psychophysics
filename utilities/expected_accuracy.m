fbffb4d910fc94aa514909a4099ee0832fc2e926
Conor Heins <conor.heins@gmail.com>
2019-07-10 12:28:38 +0200

    latest subject data
    change to expected accuracy

--- utilities/expected_accuracy.m
+++ utilities/expected_accuracy.m
@@ -2,12 +2,11 @@ function [ eAcc ] = expected_accuracy(p1,p2)
 % expected_accuracy: given two RDPs of different coherences (given by psychometric discrimiation accuracies p1 and p2), calculate the
 % expected accuracy of categorizing the scene correctly (1 of 4 scenes,
 % baseline guessing accuracy = 1/4 = 25%)
-%   Detailed explanation goes here
 
 
 accuracies = [1 0.5 0.5 0.25]; % these accuracies correspond to the situations when:
-%                       1) both RDP motions are seen 100% certainly (so scene is
-%                       100% guessable
+%                       1) both RDP motions are seen certainly (so the scene is
+%                       100% guessable)
 %                       2) the first RDP motion is seen certainly, the second is not
 %                       seen (leaving 50% guessing accuracy)
 %                       3) the second RDP motion is seen certainly, the first is
@@ -16,7 +15,11 @@ accuracies = [1 0.5 0.5 0.25]; % these accuracies correspond to the situations w
 
 perceptual_probs = [p1 * p2, p1*(1-p2), p2*(1-p1), (1-p1)*(1-p2)]; % the corresponding probabilities of the aforementioned perceptual possibilities, given the coherence levels
 
-eAcc = sum(accuracies.*perceptual_probs); % this is basically the average of the accuracies, where the weights going into the average are those given by the probabilities of each perceptual event 
+% eAcc = sum(accuracies.*perceptual_probs); % this is basically the average of the accuracies, where the weights going into the average are those given by the probabilities of each perceptual event 
     
+p = exp(-16); % to prevent numerical overflow in the log
+
+eAcc = sum(exp(log(perceptual_probs + p) + log(accuracies + p)));
+
 end
 
