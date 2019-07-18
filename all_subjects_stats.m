183a35ae3e4d02fea3b0d61ff9e11ca8aa6eadfe
Conor Heins <conor.heins@gmail.com>
2019-07-18 12:26:56 +0200

    more edits to batch analysis
    single-subjects stats stuff

--- all_subjects_stats.m
+++ all_subjects_stats.m
@@ -13,6 +13,8 @@ subj_folders(1:4) = [];
 
 stats_array = zeros(length(subj_folders),3,3,3);
 
+RT_array = zeros(length(subj_folders),3,3);
+
 for s_i = 1:length(subj_folders)
     mat_data = dir(fullfile(data_dir,subj_folders(s_i).name,'*.mat'));
     mat_names = {mat_data.name};
@@ -47,6 +49,10 @@ for s_i = 1:length(subj_folders)
         
         coh_filter_current = all_analyzed(:,coh_column) == unique_labels(lab_i);
         
+        rt_dat_temp = durations(coh_filter_current);
+        RT_array(s_i,lab_i,1) = nanmean(rt_dat_temp);
+        RT_array(s_i,lab_i,2) = nanstd(rt_dat_temp);
+        RT_array(s_i,lab_i,3) = length(rt_dat_temp);
         for lab_j = 1:length(unique_labels)
             
             coh_filter_prev = prev_cohers == unique_labels(lab_j);
@@ -56,6 +62,8 @@ for s_i = 1:length(subj_folders)
             stats_array(s_i,lab_i,2,lab_j) = nanstd(data_temp)/sqrt(N);
             stats_array(s_i,lab_i,3,lab_j) = N;
         end
+        
+        
          
     end
     
@@ -158,10 +166,11 @@ subj_folders(1:4) = [];
 
 nSubj = length(subj_folders);
 
-learning_curves = cell(1,nSubj);
 bin_size = 100; % how many trials per bin when calculating learning curves
+learning_curves = zeros(nSubj,ceil(520/bin_size));
 
 all_acc = zeros(nSubj,6);
+all_RT = zeros(nSubj,6);
 
 for s_i = 1:length(subj_folders)
     
@@ -176,16 +185,18 @@ for s_i = 1:length(subj_folders)
     
     %% collect accuracy over time (learning)
     
-    [learning_curves{s_i},~] = getLearningCurve(behav_blocks,bin_size);
+    [learning_curves(s_i,:),~,x_axis] = getLearningCurve(behav_blocks,bin_size);
     
     %% get average accuracy for different coherence conditions
     
     all_acc_temp = [];
     all_cond_temp = [];
+    all_RT_temp = [];
     
     for b_i = 2:length(behav_blocks)
         
         all_acc_temp = [all_acc_temp;[behav_blocks(b_i).trials.trialAcc]'];
+        all_RT_temp = [all_RT_temp;[behav_blocks(b_i).trials.trialRT]'];
         
         for tr = 1:length(behav_blocks(b_i).trials)
             all_cond_temp = [all_cond_temp; [behav_blocks(b_i).trials(tr).coherence(1),behav_blocks(b_i).trials(tr).coherence(2)]];
@@ -197,10 +208,126 @@ for s_i = 1:length(subj_folders)
     
     for cond_i = 1:size(unique_conds,1)
         all_acc(s_i,cond_i) = nanmean(all_acc_temp(all_cond_temp(:,1) == unique_conds(cond_i,1) & all_cond_temp(:,2) == unique_conds(cond_i,2)));
+        all_RT(s_i,cond_i) = nanmean(all_RT_temp(all_cond_temp(:,1) == unique_conds(cond_i,1) & all_cond_temp(:,2) == unique_conds(cond_i,2)));
+
     end
     
 end
-        
+
+%% plot learning curves
+
+close gcf;
+subj_colors = jet(nSubj);
+
+for s_i = 1:nSubj
+    plot(round(x_axis),learning_curves(s_i,:),'Color',subj_colors(s_i,:),'LineWidth',1.25);
+    hold on;
+end
+
+plot(round(x_axis),median(learning_curves,1),'k-.','LineWidth',5)
+
+axis tight;
+
+h = findobj(gca,'Type','line');
+
+xlabel('Trials')
+ylim([0.5 1])
+ylabel('Accuracy')
+legend(h(1),'Median (all subjects)')
+
+ax = gca;
+ax.FontSize = 24;
+
+%%
+
+unique_conds = mat2cell(nchoosek_withR([47 71 98],2),ones(6,1),2);
+
+label_handle = @(x) sprintf('%d %% + %d %%',x(1),x(2));
+
+xlabels = cellfun(@(x) label_handle(x),unique_conds,'UniformOutput',false);
+
+for s_i = 1:nSubj
+    scatter(1:length(unique_conds),all_acc(s_i,:),100,repmat(subj_colors(s_i,:),6,1),'filled');
+    hold on;
+end
+
+
+median_acc = median(all_acc,1);
+hold on; h1 = plot((1:length(unique_conds))-0.15,median_acc,'r+','MarkerSize',20,'LineWidth',5);
+legend(h1,'Median (all subjects)')
+
+xticks([1:length(unique_conds)])
+xlim([0.5 6.5])
+xticklabels(xlabels)
+xtickangle(30)
+
+ax = gca;
+ax.FontSize = 20;
+
+xlabel('Sensory uncertainty condition');
+ylabel('Accuracy')
+
+%%
+figure;
+unique_conds = mat2cell(nchoosek_withR([47 71 98],2),ones(6,1),2);
+
+label_handle = @(x) sprintf('%d %% + %d %%',x(1),x(2));
+
+xlabels = cellfun(@(x) label_handle(x),unique_conds,'UniformOutput',false);
+
+for s_i = 1:nSubj
+    scatter(1:length(unique_conds),all_RT(s_i,:),100,repmat(subj_colors(s_i,:),6,1),'filled');
+    hold on;
+end
+
+
+median_RT = median(all_RT,1);
+hold on; h1 = plot((1:length(unique_conds))-0.15,median_RT,'r+','MarkerSize',20,'LineWidth',5);
+legend(h1,'Median (all subjects)')
+
+xticks([1:length(unique_conds)])
+xlim([0.5 6.5])
+xticklabels(xlabels)
+xtickangle(30)
+
+ax = gca;
+ax.FontSize = 20;
+
+xlabel('Sensory uncertainty condition');
+ylabel('Choice latency (seconds)')
+
+%% Show main effect of coherence of current fixation on reaction time (for all subjects)
+
+figure;
+coher_names = num2cell([47 71 98]);
+
+label_handle = @(x) sprintf('%d %%',x(1));
+
+xlabels = cellfun(@(x) label_handle(x),coher_names,'UniformOutput',false);
+
+for s_i = 1:nSubj
+    scatter(1:length(coher_names),squeeze(RT_array(s_i,:,1)),100,repmat(subj_colors(s_i,:),3,1),'filled');
+    hold on;
+end
+
+median_RT = median(RT_array(:,:,1),1);
+hold on; h1 = plot((1:length(coher_names))-0.15,median_RT,'r+','MarkerSize',20,'LineWidth',5);
+legend(h1,'Median (all subjects)')
+
+xticks([1:length(unique_conds)])
+xlim([0.75 3.25])
+ylim([0.5 2.0])
+xticklabels(xlabels)
+xtickangle(30)
+
+ax = gca;
+ax.FontSize = 20;
+
+xlabel('Discrimination accuracy of current RDP');
+ylabel('Dwell time (seconds)')
+
+
+
     
     
     
--- utilities/getLearningCurve.m
+++ utilities/getLearningCurve.m
@@ -1,4 +1,4 @@
-function [ binned_acc,binned_RT ] = getLearningCurve(behav_blocks,bin_size)
+function [ binned_acc,binned_RT,x_axis ] = getLearningCurve(behav_blocks,bin_size)
 
 num_tot_trials = 0;
 for b_i = 1:length(behav_blocks)
@@ -18,13 +18,16 @@ for b_i = 1:length(behav_blocks)
 end
 
 bin_idx = 1:bin_size:num_tot_trials;
+x_axis = [];
 
 binned_acc = zeros(length(bin_idx),1);
 binned_RT = zeros(length(bin_idx),1);
 for b_i = 1:length(bin_idx)
+    x_axis = [x_axis,mean([bin_idx(b_i),min(bin_idx(b_i)+bin_size-1,num_tot_trials)])];
     binned_acc(b_i) = nanmean(all_acc(bin_idx(b_i):min(bin_idx(b_i)+bin_size-1,num_tot_trials)));
     binned_RT(b_i) = nanmean(all_RT(bin_idx(b_i):min(bin_idx(b_i)+bin_size-1,num_tot_trials)));
 end
+
 
 
 end
